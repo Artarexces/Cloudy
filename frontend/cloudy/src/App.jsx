120bcb32cd4296d856e71ab44df5455fb6579fd6
Martin Rodriguez <martinrodriguezdev96@gmail.com>
2025-06-10 20:33:48 -0300

    app

--- frontend/cloudy/src/App.jsx
+++ frontend/cloudy/src/App.jsx
@@ -4,7 +4,69 @@ import Login from './components/Login'
 import Register from './components/Register'
 
 
-const App = () => {
+const App = () => { 
+
+  const [view, setView] = useState('login')
+  const [token, setToken] = useState(localStorage.getItem('token') || null)
+
+
+//  Funcion de logeo manual
+
+  const handleLogin = async ( username, password ) => {
+    try {
+      const res = await fetch('', {
+        method: 'POST',
+        headers:{ 'Content-Type' : 'application/json' },
+        body: JSON.stringify({ username, password }),
+      })
+      if(!res.ok) throw new Error('Credenciales inválidas')
+        const data = await res.json();
+      localStorage.setItem('token',data.token)
+      setToken(data.token);
+      setView('weather');
+      return true
+    } catch (error) {
+      return false;
+    }
+  };
+
+//  Funcion de deslogeo manual
+
+  const heandleLogout = async ( ) => {
+    fetch ('', {
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json',
+        Authorization: 'Token', token        
+      }
+    }).finally (() => { 
+      localStorage.removeItem('token');
+      setToken(null)
+      setView('login')
+    });
+  };
+
+//  Funcion de registro manual
+
+  const handleRegister = async (username, password) => {
+    try {
+      const res = await fetch('', {
+        method:'POST',
+        headers: { 'Content-Type' : 'application/json'},
+        body: JSON.stringify({username, password})
+      });
+      if(!res.ok) throw new Error('Error al registrar');
+      return true;
+    } catch (error) {
+      return false;
+    }
+  };
+
+
+  if(!token && view === 'weather') {
+    setView('login')
+  }
+
   return (
     <div>App</div>
   )
--- frontend/cloudy/src/components/Weather.jsx
+++ frontend/cloudy/src/components/Weather.jsx
@@ -1,25 +1,37 @@
 import React, { useEffect, useState, useRef, useContext } from 'react'
-import { AuthContext } from '../context/AuthContext'
 import { gsap } from 'gsap'
 
 const Weather = () => {
-    const { token, logout } = useContext(AuthContext);
     const [city, setCity] = useState('');
     const [weatherData, setWeatherData] = useState(null);
-    const [forecast, setForecast] = useState(null);
+    const [forecast, setForecast] = useState([]);
     const containerRef = useRef(null);
+    const cardRef = useRef([]);
+    const weekRef = useRef([]);
 
-    useEffect(()=> {
-        gsap.set(containerRef.current, { padding: '20px' });
-    }, []);
 
     useEffect(()=>{
-        if(weatherData) {
-            gsap.from(containerRef.current, { opacity: 0, y: -50, duration: 1 });
-        }
-    }, [weatherData]);
-    
+        const clouds = containerRef.current.querySelectorAll('.cloud');
+        clouds.forEach((cloud, i) => {
+            const distance = 200 + Math.random() * 100;
+            const duration = 12 + Math.random() * 6;
+            gsap.to(cloud, {
+            x: distance,
+            duration:duration,
+            ease:'none',
+            repeat: -1,
+            yoyo: true,
+            delay: i * 1,
+            });
+        });
+    })
 
+// Effecto de bienvenida 
+
+    useEffect (() => {
+        if(!weatherData) return;
+        
+    })
     return (
     <div>Weather</div>
   )
