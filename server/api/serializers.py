c62e5ecb262d41cd7d953e8010ad5f3c0bbe0ed6
Martin Rodriguez <martinrodriguezdev96@gmail.com>
2025-09-20 11:40:25 -0300

    migracion : django a express

--- server/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import History
-
-
-class historySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = History
-        fields = ['city', 'timestamp']
