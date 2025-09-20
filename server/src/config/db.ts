8a5fca158b0e704baefd29b67eda6580de3167fe
Martin Rodriguez <martinrodriguezdev96@gmail.com>
2025-09-20 12:01:18 -0300

    add: new back estructure & mongoDb conecction1

---
+++ server/src/config/db.ts
@@ -0,0 +1,14 @@
+import { connect } from "mongoose";
+
+process.loadEnvFile();
+
+const URI_DB = process.env.URI_DB || "";
+
+export const connectDB = async () => {
+    try {
+        await connect(URI_DB!);
+        console.log("Conectado a MongoDB con exito!🍃");
+    } catch (error) {
+        console.error(`🛑 Error al conectarse al servidor de Mongo DB | ${(error as Error).message}`);
+    }
+}
