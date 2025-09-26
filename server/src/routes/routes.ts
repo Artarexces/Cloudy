675fea4366755661a64941cbcd3400d7a84a2fcc
Martin Rodriguez <martinrodriguezdev96@gmail.com>
2025-09-26 17:49:23 -0300

    bug-fix: routes2

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -7,13 +7,13 @@ import { register, login, logout } from "../controllers/authControllers";
 const router = Router()
 
 //Login & register
-router.post("/register/", register);
-router.post("/login/", login);
-router.post("/logout/",protect, logout);
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout",protect, logout);
 
 
 //Weather & forecast 
-router.get("/weather/", protect, weather);
-router.get("/history/", protect, getHistory)
+router.get("/weather", protect, weather);
+router.get("/history", protect, getHistory)
 
 export default router
